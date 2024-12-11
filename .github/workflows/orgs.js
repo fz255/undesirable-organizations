03d1a453a6f2d2b67574304b92316d003aed5c5c
Lizard <li0ard@proton.me>
2024-12-11 17:19:41 +0600

    fix error in orgs.js

--- .github/workflows/orgs.js
+++ .github/workflows/orgs.js
@@ -6,26 +6,27 @@ const agent = new https.Agent({
 });
 const fs = require('fs');
 
-let body = await fetch(`https://minjust.gov.ru/ru/documents/7756/`, {
+fetch(`https://minjust.gov.ru/ru/documents/7756/`, {
     agent: agent
 })
-body = await body.text()
-let parsed = parse(body)
-let output = []
-
-for(var i of parsed.querySelector("tbody").querySelectorAll("tr")) {
-    let obj = {
-        id: i.querySelectorAll("td")[0].textContent,
-        fullName: i.querySelectorAll("td")[3].textContent,
-        decisionInMinjust: i.querySelectorAll("td")[1].textContent,
-        decisionInGenproc: i.querySelectorAll("td")[2].textContent,
-        decisionOutMinjust: i.querySelectorAll("td")[4].textContent,
-        decisionOutGenproc: i.querySelectorAll("td")[5].textContent,
-        dateIn: (i.querySelectorAll("td")[1].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm) != null ? i.querySelectorAll("td")[1].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm)[0] : ""),
-        dateOut: (i.querySelectorAll("td")[4].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm) != null ? i.querySelectorAll("td")[4].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm)[0] : "")
+.then(res => res.text())
+.then(body => {
+    let parsed = parse(body)
+    let output = []
+    for(var i of parsed.querySelector("tbody").querySelectorAll("tr")) {
+        let obj = {
+            id: i.querySelectorAll("td")[0].textContent,
+            fullName: i.querySelectorAll("td")[3].textContent,
+            decisionInMinjust: i.querySelectorAll("td")[1].textContent,
+            decisionInGenproc: i.querySelectorAll("td")[2].textContent,
+            decisionOutMinjust: i.querySelectorAll("td")[4].textContent,
+            decisionOutGenproc: i.querySelectorAll("td")[5].textContent,
+            dateIn: (i.querySelectorAll("td")[1].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm) != null ? i.querySelectorAll("td")[1].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm)[0] : ""),
+            dateOut: (i.querySelectorAll("td")[4].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm) != null ? i.querySelectorAll("td")[4].textContent.match(/\d{2}\.\d{2}\.\d{4}/gm)[0] : "")
+        }
+        output.push(obj)
     }
-    output.push(obj)
-}
-
-fs.writeFileSync("./registry.json", JSON.stringify(output))
-console.log("Successful update")
+    
+    fs.writeFileSync("./registry.json", JSON.stringify(output))
+    console.log("Successful update")
+})
